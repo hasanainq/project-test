a36ecb14b283353e9ee8817b25820124ad2facec
Dein Name <du@example.com>
2022-07-26 10:02:40 +0200

    BUggy Code CT3

--- MyUnitTest.py
+++ MyUnitTest.py
@@ -46,12 +46,12 @@ def test_prime_number():
 
 def test_reverse():
 	value = reverse("abc") 
-	assert value == "bac"
+	assert value == "cab"
 	value = reverse([1,2,3]) 
 	assert value == [3,2,1]
 	''' add new Code Line '''
-	value = reverse("test") 
-	assert value == "text"
+	value = reverse("latex") 
+	assert value == "xetel"
 
 def test_sum():
 	sum_of_two_numbers = sum(2,2)
